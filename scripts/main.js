1a0d186d50befb4f01a7e8ca1c2f712dad189195
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-08-06 11:06:33 -0700

    More testing in app

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,20 @@ Bugsense.initAndStartSession({ apiKey: "41d8f1eb" });
 console.log('\'Allo \'Allo!');
 
 function iWouldCrash () {
-  lalala++;
+  var hey = "123123";
+  if(hey) {
+    lalala--;
+  } else {
+    lalala++;
+    lalala2++;
+    lalala++;
+    lalala1++;
+    lalala++;
+  }
+
 };
+function toCelcius(f) {
+    return (5/9) * (f-32);
+}
 
 setTimeout(iWouldCrash, 250);
